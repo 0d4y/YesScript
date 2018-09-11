12eafad47495d757f857839ff3f84b213ec8a177
0d4y <43162249+0d4y@users.noreply.github.com>
2018-09-11 05:07:10 +0000

    Add files via upload

---
+++ yesscript.php
@@ -0,0 +1,9 @@
+<html>
+ <?php
+  header('Content-Type: text/html;/json');
+ ?>
+ <body>
+  <h1>NoScript vulnerable checker</h1>
+  <script>alert('If you are using NoScript, your browser is vulnerable to YesScript!');</script>
+ </body>
+</html>
