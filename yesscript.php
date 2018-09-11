ee6421acfbf12e535a4613af7e36a686f4a30d19
0d4y <43162249+0d4y@users.noreply.github.com>
2018-09-11 05:36:40 +0000

    Update yesscript.php

--- yesscript.php
+++ yesscript.php
@@ -3,7 +3,7 @@
   header('Content-Type: text/html;/json');
  ?>
  <body>
-  <h1>NoScript vulnerable checker</h1>
+  <h1>NoScript bypass vulnerable checker</h1>
   <script>alert('If you are using NoScript, your browser is vulnerable to YesScript!');</script>
  </body>
 </html>
